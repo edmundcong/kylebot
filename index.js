3737985b1d920e35ceebc73bc097a8c151c5cf77
edmundcong1 <eddy9412@gmail.com>
2018-06-05 21:17:28 +1000

    init commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const channel = new Discord.Channel();
+const textChannel = new Discord.TextChannel(channel);
+const config = require("./config");
+const musings = require("./musings");
+
+const prodInterval = 1000 * 60 * 60;
+const testingInterval = 5000;
+
+const randomMusing = items => {
+  return items[Math.floor(Math.random() * items.length)];
+};
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const { stopMusings } = musings;
+const { generalMusings } = musings;
+const concatMusings = [...stopMusings, ...generalMusings];
+let started = false;
+let timer;
+client.on("message", msg => {
+  if (msg.content === "stop") {
+    started = false;
+    msg.reply(randomMusing(stopMusings));
+    try {
+      client.clearInterval(timer);
+    } catch (e) {}
+  } else if (msg.author.username !== "kylebot") {
+    msg.reply(randomMusing(concatMusings));
+  }
+  console.log(msg.author.username);
+  if (msg.content === "kyle" && !started) {
+    started = true;
+    timer = client.setInterval(() => {
+      msg.channel.send(randomMusing(concatMusings));
+    }, Math.floor(Math.random() * prodInterval));
+  }
+});
+
+client.login(config.token);
